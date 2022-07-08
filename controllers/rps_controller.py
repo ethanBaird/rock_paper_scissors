546815cbe59f22159aa3fe95f1ab2a849af855b2
Ethan <ethanbaird94@gmail.com>
2022-07-08 12:18:57 +0100

    mvp

---
+++ controllers/rps_controller.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+from models.game import Game
+from models.player import Player
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<p1_choice>/<p2_choice>')
+def victor(p1_choice, p2_choice):
+    rps = Game()
+    p1 = Player('Player 1', p1_choice)
+    p2 = Player('Player 2', p2_choice)
+    winner = rps.play_game(p1, p2)
+    return render_template('victor.html', winner=winner)
