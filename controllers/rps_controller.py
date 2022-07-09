2fad1aa66761e60b7a80736aad267fb14b1a6773
Ethan <ethanbaird94@gmail.com>
2022-07-09 12:58:11 +0100

    added base.html and suitable links

--- controllers/rps_controller.py
+++ controllers/rps_controller.py
@@ -3,11 +3,11 @@ from app import app
 from models.game import Game
 from models.player import Player
 
-@app.route('/')
+@app.route('/RPS')
 def index():
     return render_template('index.html')
 
-@app.route('/<p1_choice>/<p2_choice>')
+@app.route('/RPS/<p1_choice>/<p2_choice>')
 def victor(p1_choice, p2_choice):
     rps = Game()
     p1 = Player('Player 1', p1_choice)
