3356e83e8ea54c4f0584eee359d1588ac68a0fb3
Ethan <ethanbaird94@gmail.com>
2022-07-09 14:42:07 +0100

    added play computer function

--- controllers/rps_controller.py
+++ controllers/rps_controller.py
@@ -1,4 +1,6 @@
-from flask import render_template
+import random
+
+from flask import render_template, request, redirect
 from app import app
 from models.game import Game
 from models.player import Player
@@ -13,4 +15,16 @@ def victor(p1_choice, p2_choice):
     p1 = Player('Player 1', p1_choice)
     p2 = Player('Player 2', p2_choice)
     winner = rps.play_game(p1, p2)
+    return render_template('victor.html', winner=winner)
+
+@app.route('/play')
+def play():
+    return render_template('play.html')
+
+@app.route('/play', methods=['POST'])
+def vs_comp():
+    rps = Game()
+    player = Player(request.form['name'], request.form['choice'])
+    computer = Player('The Computer', random.choice(['rock', 'paper', 'scissors']))
+    winner = rps.play_game(player, computer)
     return render_template('victor.html', winner=winner)
