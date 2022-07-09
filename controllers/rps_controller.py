2cab1517f14bccdc94e40c2fce99ec1e1a9707e8
Ethan <ethanbaird94@gmail.com>
2022-07-09 15:58:36 +0100

    added lots of nice css vibes

--- controllers/rps_controller.py
+++ controllers/rps_controller.py
@@ -5,11 +5,11 @@ from app import app
 from models.game import Game
 from models.player import Player
 
-@app.route('/RPS')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/RPS/<p1_choice>/<p2_choice>')
+@app.route('/<p1_choice>/<p2_choice>')
 def victor(p1_choice, p2_choice):
     rps = Game()
     p1 = Player('Player 1', p1_choice)
